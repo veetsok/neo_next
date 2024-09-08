323f2f7640c37ae1fa861f7e214bb9e5b86d9678
veetsok <ivan.kosteev19999@yandex.ru>
2024-09-08 22:26:27 +0300

    start

---
+++ src/lib/styles/tailwind.config/colorsTW.ts
@@ -0,0 +1,13 @@
+const colorsTW = {
+  "main-background": "var(--main-background)",
+
+  "accent-blue": "rgb(var(--accent-blue) / <alpha-value>)",
+
+  "dark-primary": "rgb(var(--dark-primary) / <alpha-value>)",
+
+  "light-primary": "rgb(var(--light-primary) / <alpha-value>)",
+
+  "light-border": "rgb(var(--light-border) / <alpha-value>)",
+};
+
+export default colorsTW;
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="grid min-h-screen grid-rows-[1fr,auto]"></main>;
+}

61c0adab1245bd98c7ea9c36f4ad4a5e007b6c34
veetsok <ivan.kosteev19999@yandex.ru>
2024-09-09 02:27:51 +0300

    up

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -10,7 +10,7 @@ const ProductList: React.FC<ProductListProps> = () => {
   const renderProductSection = (title: string, variant: number) => (
     <>
       <TextAtom type={TextAtomEnum.enum_h2}>{title}</TextAtom>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
         {dataHeadphones
           .filter((e) => e.variant === variant)
           .map((product, index) => (

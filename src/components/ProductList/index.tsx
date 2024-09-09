a97ae85b0bee99d2fe1fd09aa7aad96ebb179be3
veetsok <ivan.kosteev19999@yandex.ru>
2024-09-09 03:01:30 +0300

    add fonts

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -9,7 +9,10 @@ interface ProductListProps {}
 const ProductList: React.FC<ProductListProps> = () => {
   const renderProductSection = (title: string, variant: number) => (
     <div className="flex flex-col gap-[23.89px]">
-      <TextAtom type={TextAtomEnum.enum_h2} className="text-accent-gray">
+      <TextAtom
+        type={TextAtomEnum.enum_h2}
+        className="text-accent-gray font-semibold"
+      >
         {title}
       </TextAtom>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">

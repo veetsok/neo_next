d1aea68c62da2fe1dc1fb5013b9a7122afda35c5
veetsok <ivan.kosteev19999@yandex.ru>
2024-09-21 20:06:29 +0300

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,46 @@
-import ProductList from "@/components/ProductList";
+import { dataHeadphones } from "@/assets/bd";
+import ProductCard from "@/components/items/product-card";
+import TextAtom from "@/components/ui/Atoms/Text.Atom";
+import TextAtomEnum from "@/components/ui/Atoms/Text.Atom/enum";
 
 export default function Home() {
-  return <ProductList />;
+  const renderProductSection = (title: string, variant: number) => {
+    const products = dataHeadphones.reduce((acc, product, index) => {
+      if (product.variant === variant) {
+        acc.push(
+          <ProductCard
+            key={index}
+            id={product.id}
+            image={product.image}
+            title={product.title}
+            price={product.price}
+            old={product.old}
+            rate={product.rate}
+          />
+        );
+      }
+      return acc;
+    }, [] as JSX.Element[]);
+
+    return (
+      <div className="flex flex-col gap-[23.89px]">
+        <TextAtom
+          type={TextAtomEnum.enum_h2}
+          className="text-accent-gray font-semibold"
+        >
+          {title}
+        </TextAtom>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
+          {products}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex flex-col gap-[28.35px]">
+      {renderProductSection("Наушники", 1)}
+      {renderProductSection("Беспроводные наушники", 2)}
+    </div>
+  );
 }

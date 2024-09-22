10a33f65ebe386a08df4a659453611f65fd9f130
veetsok <ivan.kosteev19999@yandex.ru>
2024-09-22 17:12:32 +0300

    add grid

--- src/components/modal/order-modal.tsx
+++ src/components/modal/order-modal.tsx
@@ -7,20 +7,55 @@ interface OrderModalProps extends Partial<CreateOrderResult> {}
 
 const OrderModal: React.FC<OrderModalProps> = ({ selectedItems, total }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full gap-7">
       <TextAtom type={TextAtomEnum.enum_h1} className="text-accent-blue">
         Спасибо за заказ
       </TextAtom>
-      {selectedItems?.map((item) => (
-        <div key={item.id} className="flex justify-between">
-          <TextAtom type={TextAtomEnum.enum_h3} className="text-accent-blue">
-            {item.title} (x{item.quantity})
+      <div className="flex flex-col gap-2">
+        <div className="grid grid-cols-6 gap-4 items-center">
+          <TextAtom
+            type={TextAtomEnum.enum_h4}
+            className="text-accent-blue col-span-4"
+          >
+            Наименование товара
           </TextAtom>
-          <TextAtom type={TextAtomEnum.enum_h3} className="text-accent-blue">
-            {item.sum} ₽
+          <TextAtom
+            type={TextAtomEnum.enum_h4}
+            className="text-accent-blue col-span-1"
+          >
+            Количество
+          </TextAtom>
+          <TextAtom
+            type={TextAtomEnum.enum_h4}
+            className="text-accent-blue col-span-1 text-right"
+          >
+            Сумма
           </TextAtom>
         </div>
-      ))}
+
+        {selectedItems?.map((item) => (
+          <div className="grid grid-cols-6 gap-4 items-center" key={item.id}>
+            <TextAtom
+              type={TextAtomEnum.enum_h3}
+              className="text-accent-blue col-span-4"
+            >
+              {item.title}
+            </TextAtom>
+            <TextAtom
+              type={TextAtomEnum.enum_h3}
+              className="text-accent-blue col-span-1"
+            >
+              x{item.quantity}
+            </TextAtom>
+            <TextAtom
+              type={TextAtomEnum.enum_h3}
+              className="text-accent-blue col-span-1 text-right"
+            >
+              {item.sum} ₽
+            </TextAtom>
+          </div>
+        ))}
+      </div>
       <TextAtom type={TextAtomEnum.enum_h3} className="text-accent-blue">
         Общая сумма: {total} ₽
       </TextAtom>
